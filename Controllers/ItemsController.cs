532dad5e9156d7696dc106a507e59d0e55c3d319
theenescresta2019 <dinesh.shrestha2019@outlook.com>
2022-05-31 09:20:38 +0545

    File Upload Completed

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,19 +25,45 @@ namespace RESTAURANT_MIS.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(ITEMS item)
+        public ActionResult Create(ITEMS item, HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+
+            try
             {
+                if (ModelState.IsValid)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var Extension = Path.GetExtension(file.FileName);
+                      //  var originalName = Path.GetFileName(file.FileName);
+                        var NewFileName = "item_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + Extension;
+                        var path = Path.Combine(Server.MapPath("~/Content/ItemImages"), NewFileName);
+                        file.SaveAs(path);
+                        item.Image = NewFileName;
+                        // filename= generatedFilename+extension
+                        // filename= attachment_20220530091512111.extension
 
-              //  item.Status = 1;
-                item.CreatedBy = "Admin";
-                item.CreatedAt = DateTime.Now.ToString();
+                    }
+                    //  item.Status = 1;
+                    item.CreatedBy = "Admin";
+                    item.CreatedAt = DateTime.Now.ToString();
 
-                db.ITEMS.Add(item);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                    db.ITEMS.Add(item);
+                    db.SaveChanges();
+                    TempData["Message"] = "Item created Successfully";
+                    TempData["MessageType"] = "Success";
+                    return RedirectToAction("Index");
+                }
+              
             }
+            catch (Exception ex)
+            {
+                TempData["Message"] = "Unable to create new item. Error: "+ex.Message;
+                TempData["MessageType"] = "Error";
+                return View();
+            }
+            
+        
             return RedirectToAction("Index");
         }
 
